0c41f71333609f91c184b748341cafebc18c0fdd
am47460 <am47460@logti-a3412-45>
2016-06-16 09:48:04 -0400

    Added skeleton class

--- LOG121-TP3/src/framework/IStrategie.java
+++ LOG121-TP3/src/framework/IStrategie.java
@@ -2,4 +2,7 @@ package framework;
 
 public interface IStrategie {
 
+	public Joueur calculerLeVainqueur(final Jeu aJeu);
+	
+	public int calculerScoreTour(final Jeu aJeu);
 }
--- LOG121-TP3/src/framework/Jeu.java
+++ LOG121-TP3/src/framework/Jeu.java
@@ -1,5 +1,17 @@
 package framework;
 
-public class Jeu {
+public class Jeu implements IStrategie{
+
+	@Override
+	public Joueur calculerLeVainqueur(Jeu aJeu) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int calculerScoreTour(Jeu aJeu) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
