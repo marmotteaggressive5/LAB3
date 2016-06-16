0ebc9f3d476e4cd06a6f4d4a82f73b33c7751fd5
am47460 <am47460@logti-a3412-45>
2016-06-16 09:55:15 -0400

    Test sous

--- LOG121-TP3/src/framework/Jeu.java
+++ LOG121-TP3/src/framework/Jeu.java
@@ -3,14 +3,16 @@ package framework;
 public class Jeu implements IStrategie{
 
 	@Override
-	public Joueur calculerLeVainqueur(Jeu aJeu) {
+	public Joueur calculerLeVainqueur(final Jeu aJeu) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int calculerScoreTour(Jeu aJeu) {
+	public int calculerScoreTour(final Jeu aJeu) {
 		// TODO Auto-generated method stub
+		int somme = 1+1;
+		int sous = 1-1;
 		return 0;
 	}
 
