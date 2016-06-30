0334badc8161497345d49fa507cb1c63bb29322a
am47460 <am47460@logti-a3412-45>
2016-06-30 09:31:26 -0400

    added method to rouler De

--- LOG121-TP3/src/framework/De.java
+++ LOG121-TP3/src/framework/De.java
@@ -2,10 +2,20 @@ package framework;
 
 public class De implements Comparable<De> {
 
+	private int nbFaces;
+	
+	public De(final int aNbDe) {
+		this.nbFaces = aNbDe;
+	}
+	
 	@Override
 	public int compareTo(De de) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
+	public int roulerDe() {
+		return (int) (Math.random()*nbFaces + 1);
+	}
+	
 }
